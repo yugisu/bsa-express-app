c7c3646f610c260685256cb513826aabdec9bc45
yugisu <slymahel@gmail.com>
2019-05-24 19:48:04 +0300

    Implement /users routing with data from file

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,10 +1,23 @@
 import { Router } from 'express';
+import { getUsers, getUserById, addUser } from '../repositories';
 
-const usersRouter = Router();
+const router = Router();
 
 /* GET users listing. */
-usersRouter.get('/', (req, res, next) => {
-  res.send('*list of users here*');
+router.get('/', function(req, res, next) {
+  getUsers((data) => res.send(data));
 });
 
-export { usersRouter };
+/* GET user by id */
+router.get('/:id', function(req, res, next) {
+  const userId = req.params.id;
+
+  getUserById(userId, (data) => res.send(data));
+});
+
+/* POST new user */
+router.post('/', function(req, res, next) {
+  addUser(req.body, (data) => res.send(data));
+});
+
+export const usersRouter = router;
